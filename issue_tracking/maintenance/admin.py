2f767444e657d6e7ce971b7f1690b6629d4fcf12
ZorexSalvo <zsalvo@ayannah.com>
2016-09-29 23:29:00 +0800

    Refactor all the files
    This changes includes modification of the admin,
    creation of new models, addition of validators,
    etc.
    
    Signed-off-by: ZorexSalvo <zsalvo@ayannah.com>

--- issue_tracking/maintenance/admin.py
+++ issue_tracking/maintenance/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
-from maintenance.models import Issue, Representative, Ticket, Repository
+from maintenance.models import User, Issue, Ticket
 
 
 class TicketInline(admin.StackedInline):
@@ -12,25 +12,53 @@ class TicketInline(admin.StackedInline):
 
 class IssueAdmin(admin.ModelAdmin):
     inlines = [TicketInline,]
-    search_fields = ('reference_number', 'title', 'created_by')
-    list_display = ('reference_number', 'title', 'created_by', 'date_created')
-    readonly_fields = ('date_created',)
+    search_fields = ('title', 'created_by', 'reference_id')
+    list_display = ('reference_id', 'title', 'created_by', 'date_created')
+    fields = ('title', 'created_by', 'date_created')
+    readonly_fields = ('date_created', 'created_by')
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'author', None) is None:
+            obj.created_by = request.user
+        obj.save()
+
+    def get_queryset(self, request):
+        qs = super(IssueAdmin, self).get_queryset(request)
+        qs = qs.order_by('-id')
+        return qs
 
 
-class RepresentativeAdmin(admin.ModelAdmin):
+class UserAdmin(admin.ModelAdmin):
     inlines = [TicketInline,]
-    search_fields = ('reference_number', 'first_name', 'middle_name', 'last_name')
-    list_display = ('first_name', 'middle_name', 'last_name')
+    search_fields = ('first_name', 'middle_name', 'last_name')
+    list_display = ('first_name', 'middle_name', 'last_name', 'sex', 'date_of_birth')
+    readonly_fields = ('date_created', 'created_by')
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'author', None) is None:
+            obj.created_by = request.user
+        obj.save()
 
 
 class TicketAdmin(admin.ModelAdmin):
-    search_fields = ('representative', 'issue', 'reference_number', 'remark')
-    list_display = ('issue', 'representative', 'reference_number', 'remark', 'created_by', 'date_created')
+    search_fields = ('assigned_to', 'issue', 'reference_id', 'remark')
+    list_display = ('reference_id', 'issue', 'assigned_to', 'remark', 'created_by', 'date_created')
+    fields = ('issue', 'assigned_to', 'remark', 'note', 'created_by', 'date_created')
+    readonly_fields = ('date_created', 'created_by')
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'author', None) is None:
+            obj.created_by = request.user
+        obj.save()
+
+    def get_queryset(self, request):
+        qs = super(TicketAdmin, self).get_queryset(request)
+        qs = qs.order_by('-id')
+        return qs
 
 
 admin.site.unregister(Group)
 admin.site.register(Issue, IssueAdmin)
-admin.site.register(Representative, RepresentativeAdmin)
 admin.site.register(Ticket, TicketAdmin)
-admin.site.register(Repository)
+admin.site.register(User, UserAdmin)
 admin.site.site_header = 'Issue Tracking System'
--- issue_tracking/maintenance/models.py
+++ issue_tracking/maintenance/models.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from django.contrib.auth.models import User as AuthUser
 from django.core.validators import RegexValidator
 from django.db import models
 
@@ -6,30 +7,36 @@ from django.db import models
 PHONE_REGEX = RegexValidator(
     regex=r'^\b(09)\d{9}?\b$',
     message='Phone number must be entered in the format: 09XXXXXXXXXX.')
+NAME_REGEX = RegexValidator(
+    regex=r'^[a-zA-Z\xd1\xf1\s.-]*$',
+message='Invalid input.')
 
 
 class Issue(models.Model):
-    reference_number = models.CharField(max_length=200, unique=True)
+    reference_id = models.CharField(max_length=200, unique=True, blank=True)
     title = models.CharField(max_length=200)
     date_created = models.DateTimeField(auto_now_add=True)
     created_by = models.CharField(max_length=200)
 
     def __str__(self):
-        return '{} - {}'.format(self.reference_number, self.title)
+        return '{0} - {1}'.format(self.reference_id, self.title)
+
+    def save(self, *args, **kwargs):
+        super(Issue, self).save(*args, **kwargs)
+        Issue.objects.filter(id=self.id).update(reference_id='#{0:04d}'.format(self.id))
 
 
-class Representative(models.Model):
+class User(models.Model):
     SEX = (('MALE', 'Male'),
            ('FEMALE', 'Female'))
 
-    reference_number = models.CharField(max_length=200, unique=True)
-    first_name = models.CharField(max_length=200)
-    middle_name = models.CharField(max_length=200, blank=True)
-    last_name = models.CharField(max_length=200)
+    username = models.OneToOneField(AuthUser, related_name='auth_user')
+    first_name = models.CharField(max_length=200, validators=[NAME_REGEX])
+    middle_name = models.CharField(max_length=200, blank=True, validators=[NAME_REGEX])
+    last_name = models.CharField(max_length=200, validators=[NAME_REGEX])
     date_of_birth = models.DateField()
-    sex = models.CharField(max_length=200, choices=SEX, default='Male')
+    sex = models.CharField(max_length=200, choices=SEX, default='MALE')
     mobile_number = models.CharField(max_length=13, validators=[PHONE_REGEX])
-    email_address = models.EmailField(max_length=200, blank=True, unique=True)
     date_created = models.DateTimeField(auto_now_add=True)
     created_by = models.CharField(max_length=200)
 
@@ -43,16 +50,21 @@ class Ticket(models.Model):
                ('HIGH', 'High'),
                ('EMERGENCY', 'Emergency'))
 
-    representative = models.ForeignKey(Representative, related_name='ticket_repr')
+    assigned_to = models.ForeignKey(User, related_name='ticket_repr')
     issue = models.ForeignKey(Issue, related_name='ticket_issue')
-    reference_number = models.CharField(max_length=200, unique=True)
+    reference_id = models.CharField(max_length=200, unique=True)
     remark = models.CharField(max_length=200, choices=REMARKS, default='NORMAL')
     note = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     created_by = models.CharField(max_length=200)
 
     def __str__(self):
-        return '{} - {}'.format(self.reference_number, self.representative)
+        return '{} - {}'.format(self.reference_id, self.assigned_to)
+
+    def save(self, *args, **kwargs):
+        super(Ticket, self).save(*args, **kwargs)
+        Ticket.objects.filter(id=self.id).update(reference_id='#{0:04d}'.format(self.id))
+
 
 
 class Repository(models.Model):
