aae2a56dc5abd3fdaf656a0a751d9ac79a77d5e7
Zorex Salvo <zorexsalvo@gmail.com>
2016-11-30 22:26:52 +0800

    Create a view for the login page
    Add the maintenance.urls to project url.
    
    Signed-off-by: Zorex Salvo <zorexsalvo@gmail.com>

--- issue_tracking/maintenance/views.py
+++ issue_tracking/maintenance/views.py
@@ -1,3 +1,21 @@
+from django.contrib.auth import login, logout
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from . forms import *
 
-# Create your views here.
+class LoginView(TemplateView):
+    form_class = LoginForm
+    template_name = 'security/login.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            user = form.login(request)
+            login(request, user)
+            return HttpResponseRedirect('/')
+        return render(request, self.template_name, {'form': form})
