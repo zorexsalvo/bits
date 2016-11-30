2f6b2dfdc280c9b0f93b06696f71392ef3ef0ff6
Zorex Salvo <zorexsalvo@gmail.com>
2016-11-30 22:26:03 +0800

    Create a LoginForm
    Signed-off-by: Zorex Salvo <zorexsalvo@gmail.com>

---
+++ issue_tracking/maintenance/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username','class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'form-control'}))
+
+    def clean(self):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        if not user or not user.is_active:
+            raise forms.ValidationError('Invalid credentials.')
+        return self.cleaned_data
+
+    def login(self, request):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
