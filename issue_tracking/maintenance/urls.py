b4e1548c05d8128c7cc0ae825faf181fc1f313eb
Zorex Salvo <zorexsalvo@gmail.com>
2016-12-03 10:12:04 +0800

    Create view for creating company and for viewing employees
    Signed-off-by: Zorex Salvo <zorexsalvo@gmail.com>

--- issue_tracking/maintenance/urls.py
+++ issue_tracking/maintenance/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     url(r'login/$', LoginView.as_view(), name='login'),
     url(r'logout/$', logout_view, name='logout'),
+    url(r'create_company/$', CreateCompany.as_view(), name='create_company'),
+    url(r'view_employee/$', ViewEmployee.as_view(), name='view_employee')
 ]
--- issue_tracking/maintenance/views.py
+++ issue_tracking/maintenance/views.py
@@ -1,8 +1,11 @@
+import time
 from django.contrib.auth import login, logout
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from . forms import *
+from django.views import View
+from .forms import *
+from .models import Company, User
 
 
 def logout_view(request):
@@ -23,7 +26,58 @@ class LoginView(TemplateView):
         if form.is_valid():
             user = form.login(request)
             _login = login(request, user)
-            print('=============')
-            print(_login)
-            return HttpResponseRedirect('/')
+            return HttpResponseRedirect('/create_company/')
         return render(request, self.template_name, {'form': form})
+
+
+class AdministratorView(TemplateView):
+    def get_companies(self):
+        return Company.objects.all()
+
+    def get_user(self, request):
+        return  User.objects.filter(username=request.user).first()
+
+
+class CreateCompany(AdministratorView):
+
+    form_class = CreateCompanyForm
+    template_name = 'administrator/create_company.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        form = self.form_class()
+        context['form'] = form
+        context['companies'] = self.get_companies()
+        context['user'] = self.get_user(request)
+        return render(request, self.template_name, context)
+
+    def post(self, request, *args, **kwargs):
+        context = {}
+        form = self.form_class(request.POST or None)
+        context['request'] = 'POST'
+        context['companies'] = self.get_companies()
+        context['user'] = self.get_user(request)
+
+        if form.is_valid():
+            name = form.cleaned_data.get('name')
+            company = Company(name=name)
+            company.save()
+            context['form'] = self.form_class()
+            return render(request, self.template_name, context)
+
+        context['form'] = form
+        return render(request, self.template_name, context)
+
+
+class ViewEmployee(AdministratorView):
+    template_name = 'administrator/view_employee.html'
+
+    def get_employees(self):
+        return User.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        context['companies'] = self.get_companies()
+        context['employees'] = self.get_employees()
+        context['user'] = self.get_user(request)
+        return render(request, self.template_name, context)
