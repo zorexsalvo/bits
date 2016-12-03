1f2bbe257e6ddb887cda647ee6dbc14bd974dd2a
Zorex Salvo <zorexsalvo@gmail.com>
2016-12-03 23:44:58 +0800

    Require login for accessing the pages
    Signed-off-by: Zorex Salvo <zorexsalvo@gmail.com>

--- issue_tracking/maintenance/urls.py
+++ issue_tracking/maintenance/urls.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
     url(r'login/$', LoginView.as_view(), name='login'),
-    url(r'logout/$', logout_view, name='logout'),
-    url(r'create_company/$', CreateCompany.as_view(), name='create_company'),
-    url(r'view_employee/$', ViewEmployee.as_view(), name='view_employee'),
-    url(r'create_employee/$', CreateEmployee.as_view(), name='create_employee')
+    url(r'logout/$', login_required(logout_view), name='logout'),
+    url(r'create_company/$', login_required(CreateCompany.as_view()), name='create_company'),
+    url(r'view_employee/$', login_required(ViewEmployee.as_view()), name='view_employee'),
+    url(r'create_employee/$', login_required(CreateEmployee.as_view()), name='create_employee')
 ]
