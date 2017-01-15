97f82b70c49b3db9d8d5148ec4d9555ef10ead8e
Zorex Salvo <zorexsalvo@gmail.com>
2017-01-15 14:25:28 +0800

    Create test for logging in

--- issue_tracking/record/tests.py
+++ issue_tracking/record/tests.py
@@ -1,5 +1,11 @@
 from django.test import TestCase
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+
+from django.core.urlresolvers import reverse
+
 from .models import Company
+from .forms import LoginForm, CompanyForm
 
 class ModelTestCase(TestCase):
     def setUp(self):
@@ -8,4 +14,17 @@ class ModelTestCase(TestCase):
     def test_create_company(self):
         company = Company(name='My Company')
         company.save()
+
+
+class LoginTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create_user(username='juan',
+                                        password='password123',
+                                        email='juan@bits.com')
+
+    def test_login(self):
+        form = LoginForm({'username': 'juan', 'password': 'password123'})
+
+        self.assertTrue(form.is_valid())
+        self.assertIsNotNone(authenticate(username='juan', password='password123'))
 
